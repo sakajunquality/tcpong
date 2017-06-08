151a47b56535ddea09f86dd2a03dfb77616597b9
Jun SAKATA <jun.sakata@eure.jp>
2017-06-08 10:19:44 +0900

    init

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sakajunquality/tcpong/ping"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "tcpong"
+	app.Usage = "Ping over TCP"
+
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "i",
+			Value: 1,
+			Usage: "Request Interval (Sec)",
+		},
+		cli.IntFlag{
+			Name:  "t",
+			Value: 1,
+			Usage: "Request Timeout (Sec)",
+		},
+		cli.StringFlag{
+			Name:  "p",
+			Value: "tcp",
+			Usage: "Protocol",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if c.NArg() != 2 {
+			return cli.NewExitError("illegal number of args", 2)
+		}
+
+		hostString, portString := c.Args().Get(0), c.Args().Get(1)
+		// to do validate
+
+		ch := make(chan string)
+		var seq int
+
+		t := ping.Target{
+			Protocol: c.GlobalString("p"),
+			Host:     hostString,
+			Port:     portString,
+			Timeout:  time.Duration(c.GlobalInt("t")) * time.Second,
+		}
+
+		for {
+			go func() {
+				r, err := t.Dial()
+				seq++
+
+				if err != nil {
+					ch <- fmt.Sprintf("Error: %s", err)
+					return
+				}
+
+				ch <- fmt.Sprintf("state=%s fromr=%s rtt=%s", r.Info.State, r.RemoteAddr, r.Info.RTT)
+			}()
+
+			fmt.Printf("tcp_seq=%d %s\n", seq, <-ch)
+			time.Sleep(time.Duration(c.GlobalInt("i")) * time.Second)
+		}
+	}
+
+	app.Run(os.Args)
+}
---
+++ ping/dial.go
@@ -0,0 +1,53 @@
+package ping
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/mikioh/tcp"
+	"github.com/mikioh/tcpinfo"
+)
+
+type Target struct {
+	Protocol string
+	Host     string
+	Port     string
+	Timeout  time.Duration
+}
+
+type Res struct {
+	RemoteAddr string
+	LocalAddr  string
+	Info       *tcpinfo.Info
+}
+
+func (t *Target) Dial() (Res, error) {
+	r := Res{}
+	network := fmt.Sprintf("%s:%s", t.Host, t.Port)
+	c, err := net.DialTimeout(t.Protocol, network, t.Timeout)
+	defer c.Close()
+
+	if err != nil {
+		return r, err
+	}
+
+	r.RemoteAddr = c.RemoteAddr().String()
+	r.LocalAddr = c.LocalAddr().String()
+
+	tc, err := tcp.NewConn(c)
+	if err != nil {
+		return r, err
+	}
+
+	var o tcpinfo.Info
+	var b [256]byte
+	i, err := tc.Option(o.Level(), o.Name(), b[:])
+	if err != nil {
+		return r, err
+	}
+
+	r.Info = i.(*tcpinfo.Info)
+
+	return r, nil
+}
